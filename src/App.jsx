1d42ca303432be5757f68daeb9cd31ddc5c5adf6
Abhishek Pattanayak <2021.abhishek.pattanayak@ves.ac.in>
2024-07-07 02:34:05 +0530

    Fix: theme toggle using context api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { useEffect } from "react"
+import { useState } from "react"
+import { createContext } from "react"
+import Header from "./components/Header"
+
+export const ThemeContext = createContext()
 
 export default function App () {
+  const [prefersDarkTheme, setPrefersDarkTheme] = useState(true)
+
+  const toggleTheme = () => setPrefersDarkTheme(prev => !prev)
+
+  useEffect(()=>{
+    document.documentElement.classList.toggle('dark')
+    console.log('hua')
+  }, [prefersDarkTheme])
 
   return (
-    <div className="min-h-screen dark:bg-black font-mono ">
-
-    </div>
+    <ThemeContext.Provider value={{prefersDarkTheme, toggleTheme}}>
+      <div className="min-h-screen dark:bg-black dark:text-white" >
+        <Header />
+      </div>
+    </ThemeContext.Provider>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,21 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import navbarSvg from '../assets/header-bars.svg'
 import crossSvg from '../assets/cross.svg'
-
+import { ThemeContext } from "../App";
+import sunSvg from '../assets/sun.svg'
+import moonSvg from '../assets/moon.svg'
 
 export default function Header () {
   const bool = window.innerWidth >= 640
   const [disp, setDisp] = useState(bool);
 
+  const {prefersDarkTheme, toggleTheme} = useContext(ThemeContext)
+  const [themeIcon, setThemeIcon] = useState(() => prefersDarkTheme?sunSvg:moonSvg)
+  
+  useEffect(()=>{
+    setThemeIcon(() => prefersDarkTheme?sunSvg:moonSvg)
+  }, [prefersDarkTheme])
 
   useEffect(()=>{
     const handleResize = () => {
@@ -27,38 +35,40 @@ export default function Header () {
   ]
 
   return (
-    <nav className="sticky top-0 py-2 px-2 dark:bg-black dark:text-white sm:py-4 ">
+      <nav className="sticky top-0 py-2 px-2 dark:bg-black dark:text-white sm:py-4 ">
       {disp ? 
-      <ul className="flex flex-col sm:flex-row items-center sm:justify-end sm:gap-2 sm:px-4 sm:text-lg md:gap-4 md:px-6 md:text-xl lg:gap-6 lg:px-8 lg:text-2xl dark:bg-black">
-        <li className="self-end sm:hidden">
-          <button onClick={()=>setDisp(false)} className="dark:bg-white h-full rounded-md">
-            <img src={crossSvg} className="h-full" alt="" />
-          </button>
-        </li>
+        <ul className="flex flex-col sm:flex-row items-center sm:justify-end sm:gap-2 sm:px-4 sm:text-lg md:gap-4 md:px-6 md:text-xl lg:gap-6 lg:px-8 lg:text-2xl dark:bg-black">
 
-        {links.map((li, index) => 
-          <li className="flex content-center" key={index}>
-            <Link className="h-full" to={li.to}>{li.content}</Link>
+          <li className="self-end sm:hidden">
+            <button onClick={()=>setDisp(false)} className="dark:bg-white h-full rounded-md">
+              <img src={crossSvg} className="h-full" alt="" /> 
+            </button>
           </li>
-        )}
-        <li>
-          {/* theme button */}
-          <button className="rounded-md px-2 py-1 h-8 md:h-10 xl:h-12 font-bold">
-            <img className="h-full stroke-black dark:stroke-white" alt="" />
-          </button>
-        </li>
 
-      </ul>
+          {/* {links.map((li, index) => 
+            <li className="flex content-center" key={index}>
+              <Link className="h-full" to={li.to}>{li.content}</Link>
+            </li>
+          )} */}
+
+          <li>
+            {/* theme button */}
+            <button className="rounded-md px-2 py-1 h-8 md:h-10 xl:h-12 font-bold" onClick={toggleTheme}>
+              <img className="h-full" src={themeIcon} alt="" />
+            </button>
+          </li>
+
+        </ul>
 
       :
-        // cross button
-      <button 
+      // cross button
+        <button 
         className="p-0.5 rounded-sm grid place-content-center aspect-square h-8 border border-black dark:border-white sm:hidden dark:bg-white "
         onClick={()=>setDisp(true)}
-      >
+        >
         <img src={navbarSvg} alt="" />
-      </button> 
+        </button> 
       }
-    </nav>
+      </nav>
   )
 }
