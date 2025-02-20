8469fb4bcdeba0ec3fbaa6c2de58b65c3b48f11c
Abhishek Pattanayak <2021.abhishek.pattanayak@ves.ac.in>
2025-02-20 19:55:05 +0530

    feat: add typing effect

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,11 @@
-import { toggleTheme } from "./utils/toggleTheme.ts";
+import Hero from "./components/hero.tsx";
 
 // @deno-types="@types/react"
 
 function App() {
 	return (
-		<div className="h-screen dark:bg-black dark:text-white">
-			<button onClick={toggleTheme}>Toggle</button>
+		<div className="min-h-screen dark:bg-black dark:text-white">
+			<Hero />
 		</div>
 	);
 }
---
+++ app/src/components/hero.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState, useMemo } from "react";
+
+export default function Hero() {
+	const [blink, setBlink] = useState(true);
+	const text: string = useMemo(() => "Hi! I am Abhishek ..", []);
+	const [typing, setTyping] = useState<boolean>(false);
+	const [displayText, setDisplayText] = useState<string>("");
+
+	useEffect(() => {
+		const timerArray: number[] = [];
+		const typingSpeed = 50;
+
+		setDisplayText("");
+		setTyping(true);
+
+		for (let i = 0; i < text.length; ++i) {
+			const timer = setTimeout(() => {
+				setDisplayText((prev: string) => prev + text[i]);
+			}, typingSpeed * i);
+			timerArray.push(timer);
+		}
+
+		const timer = setTimeout(() => {
+			setTyping(false);
+		}, typingSpeed * text.length);
+		timerArray.push(timer);
+
+		return () => {
+			timerArray.forEach(clearTimeout);
+		};
+	}, [text]);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setBlink((prev: boolean) => !prev);
+		}, 750);
+		return () => clearInterval(interval);
+	}, []);
+
+	return (
+		<>
+			<div className="h-screen grid place-content-center font-light">
+				<h1 className="text-md md:text-xl selection:bg-purple-400 selection:text-white ">
+					<span>{displayText}</span>
+					{!typing && <span className={`${blink && "text-black"}`}>.</span>}
+				</h1>
+			</div>
+		</>
+	);
+}
