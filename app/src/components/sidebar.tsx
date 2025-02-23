a29a9f22ba5b636061c8cea6ec46043ae70e8982
Abhishek Pattanayak <2021.abhishek.pattanayak@ves.ac.in>
2025-02-23 20:54:26 +0530

    feat: implement opening new tab for components in sidebar

--- app/src/components/sidebar.tsx
+++ app/src/components/sidebar.tsx
@@ -1,3 +1,4 @@
+import { openNewTab } from "../utils/utils.ts";
 import Githubsvg from "./svg/githubsvg.tsx";
 import LeetcodeSvg from "./svg/leetcodesvg.tsx";
 import LinkedinSvg from "./svg/linkedinsvg.tsx";
@@ -7,9 +8,15 @@ export default function Sidebar() {
 	return (
 		<div className="fixed bottom-0 left-0 w-fit flex flex-col-reverse items-center m-2 gap-2 *:hover:scale-105 *:h-5 *:w-5 *:hover:cursor-pointer">
 			<ThemeToggler />
-			<Githubsvg />
-			<LinkedinSvg />
-      <LeetcodeSvg />
+			<LeetcodeSvg
+				onClick={openNewTab("https://leetcode.com/u/abhishek-pattanayak/")}
+			/>
+			<LinkedinSvg
+				onClick={openNewTab("https://www.linkedin.com/in/abhishekppattanayak")}
+			/>
+			<Githubsvg
+				onClick={openNewTab("https://github.com/abhishekppattanayak")}
+			/>
 		</div>
 	);
 }
---
+++ app/src/utils/utils.ts
@@ -0,0 +1,3 @@
+export function openNewTab(url: string) {
+  return () => globalThis.open(url, "_blank");
+}
